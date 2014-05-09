1376d4540de77f74d329946bc9be581d14768a01
Thomas Getgood <thomas.getgood@gmail.com>
2014-05-09 17:05:55 +0530

    A server.

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject travels "v0.0.1"
+  
+  :description "Unknown"
+
+  :url "N/A"
+
+  :min-lein-version "2.0.0"
+
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [com.cemerick/friend "0.2.0"]
+                 [compojure "1.1.6"]
+                 [http-kit "2.1.18"]
+                 [ring/ring-json "0.3.1"]]
+
+  :source-path "src/clj"
+
+  :main travels.server
+)
+                 
---
+++ src/clj/travels/server.clj
@@ -0,0 +1,20 @@
+(ns travels.server
+  (:require [ring.middleware.json :refer [wrap-json-response wrap-json-body]]
+            [org.httpkit.server :refer [run-server]]
+            [compojure.core :refer :all]
+            [compojure.route :as route]
+            [compojure.handler :as ch]
+            [cemerick.friend :as friend]
+            [cemerick.friend [workflows :as workflows]
+                             [credentials :as creds]]))
+
+(defroutes site-routes
+  ;; The App is all server statically. We might consider a CDN. 
+  (GET "/" [] (response/redirect "/index.html"))
+  (route/resources ""))
+
+(defn -main
+  (let [opts {:port 8000}]
+    (run-server site-routes opt)
+    (println (str "Server is running on port: " (:port opts)))))
+           
