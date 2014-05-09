36d0c407997e2ccc7609004fc6303011a2638149
Thomas Getgood <thomas.getgood@gmail.com>
2014-05-09 16:04:15 +0530

    Initial.

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+///// Extensions to Ember itself
+//=================================
+ 
+// Taken from: https://gist.github.com/mehulkar/3232255.
+// No license specified...
+Ember.ArrayProxy.prototype.flatten = Array.prototype.flatten = function() {
+    var r = [];
+    this.forEach(function(el) {
+	r.push.apply(r, Ember.isArray(el)  ? el.flatten() : [el]);
+    });
+    return r;
+};
+
+
+// App
+//=================================
+
+App = Ember.Application.create();
+
+App.IndexRoute = Ember.Route.extend({});
