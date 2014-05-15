ba1d8050deb80cf6b08e9718b495e827029d49e5
Thomas Getgood <thomas.getgood@gmail.com>
2014-05-15 16:37:36 +0530

    Getting fed up with postgres...

--- src/clojure/travels/api.clj
+++ src/clojure/travels/api.clj
@@ -1,28 +1,22 @@
 (ns travels.api
   (:require [travels.util :refer [ember-response]]
-            [travels.database :refer [db]]
-            [clojure.edn :as edn]
-            [korma.core :as k :refer :all]))
-
-(defentity sights
-  (pk :id)
-  (database db)
-  (entity-fields :name :description :flagship_photo :additional_photos))
+            [travels.database :refer [db sight photo]]
+            [clojure.edn :as edn]))
 
 (defn create-sight
   [data]
   ;; Validate!!!!!
   (ember-response :id
-   (insert sights
+   (insert sight
      (values (get data "sight")))))
 
 (defn get-sights
   []
   (ember-response :sights
-    (select sights)))
+    (select sight)))
 
-(defn get-sight
+(defn get-sightf
   [id]
   (ember-response :sight
-  (select sights
+  (select sight
     (where {:id (edn/read-string id)}))))
--- src/clojure/travels/database.clj
+++ src/clojure/travels/database.clj
@@ -2,45 +2,87 @@
   (:require [clojure.java.jdbc :as sql]
             [clojure.string :as string]
             [korma.db :refer [defdb postgres]]
+            [korma.core :as k :refer :all]
             [travels.config :as config]))
+
+;;;;; DB init.
+
+(def db
+  (postgres
+   (cond config/devdb?
+         {:classname "org.postgresql.Driver"
+          :subprotocol "postgresql"
+          :subname "//localhost:5432/traveldb"}
+         config/proddb?
+         ;; ghetto ass URL parsing cause connection-uri doesn't work.
+         (let [re #"postgres://([^:\s]+):([^:@\s]*)@(.*)"
+               match (re-find re config/db-uri)]
+           (println match)
+           {:classname   "org.postgresql.Driver"
+            :subprotocol "postgresql"
+            :subname     (str "//" (nth match 3))
+            :user        (nth match 1)
+            :password    (nth match 2)
+            ;; :ssl         true ; Eventually needed
+            :make-pool?  true})
+         :else
+         (throw (Exception. "Don't know which database to connect to.")))))
+
+;;;;; Schemata
 
 (def sight-schema
   {:name              :text
    :id                :bigserial
    :description       :text
    :address           :text
-   :geocoordinates   :point
+   :geocoordinates    :point
    :location          :text
-   :flagship_photo    :text
-   :additional_photos "text[]"
    :created           :timestamptz
    :last_modified     :timestamptz})
-   
 
-(def db
-  (postgres
-   (cond config/devdb?
-           {:classname "org.postgresql.Driver"
-            :subprotocol "postgresql"
-            :subname "//localhost:5432/traveldb"}
-         config/proddb?
-                ;; ghetto ass URL parsing cause connection-uri doesn't work.
-           (let [re #"postgres://([^:\s]+):([^:@\s]*)@(.*)"
-                 match (re-find re config/db-uri)]
-             (println match)
-             {:classname   "org.postgresql.Driver"
-              :subprotocol "postgresql"
-              :subname     (str "//" (nth match 3))
-              :user        (nth match 1)
-              :password    (nth match 2)
-              ;; :ssl         true ; Eventually needed
-              :make-pool?  true})
-         :else
-           (throw (Exception. "Don't know which database to connect to.")))))
+(def photo-schema
+  {:id            :bigserial
+   :sight_id      :bigint
+   :link          :text
+   :primary_photo :bool
+   :created       :timestamptz})
+
+;;;;; create tables. Where should this be?
+
+(defn -wipe-tables!
+  []
+  (when config/devdb?
+    (sql/db-do-commands
+     db
+     (sql/drop-table-ddl :sights)
+     (sql/drop-table-ddl :photos))))
 
 (defn -create-tables!
   []
   (sql/db-do-commands
    db
-   (apply (partial sql/create-table-ddl :sights) sight-schema))) 
+   (apply (partial sql/create-table-ddl :sights) sight-schema)
+   (apply (partial sql/create-table-ddl :photos) photo-schema))) 
 
+;;;;; entities
+
+(declare sight photo)
+
+(defentity sight
+  (pk :id)
+  (database db)
+  (table :sights)
+  (entity-fields :name :description :photos)
+  (has-many photo {:fk :sight_id}))
+
+(defentity photo
+  (pk :id)
+  (database db)
+  (table :photos)
+  (belongs-to sight))
+
+(def test-sight
+  {:name "some"
+   :photos [{:link "sdfdsf" :primary_photo false}]
+   })
+ 
