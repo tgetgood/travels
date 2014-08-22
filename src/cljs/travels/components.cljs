547b77bb51ba91e93146c473e04f0d46ae6b5e08
Thomas Getgood <thomas.getgood@gmail.com>
2014-08-21 23:45:55 -0400

    Reimplemented 90% in Om.

--- src/cljs/travels/components.cljs
+++ src/cljs/travels/components.cljs
@@ -1,6 +1,8 @@
 (ns travels.components
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
-  (:require [cljs.core.async :refer [<! >! put! chan]]
+  (:require [travels.gmaps :as gm]
+            
+            [cljs.core.async :refer [<! >! put! chan]]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]))
 
@@ -9,8 +11,8 @@
     {:sites []
      :accepted []
      :rejected []
-     :index 0
      :selected {}
+     :markers []
      :map-canvas nil}))
 
 (defn travel-bar
@@ -41,7 +43,7 @@
     om/IRenderState
     (render-state 
       [this {:keys [focus]}]
-      (dom/div #js {:onMouseover (fn [ev] (put! focus @app))}
+      (dom/div #js {:onMouseOver (fn [_] (go (>! focus @app)))}
         (dom/h2  #js {:className "site-name"} (:name app))
         (dom/img #js {:className "main-image" :src (get-std-image app)})
         (om/build travel-bar app)))))
@@ -56,36 +58,71 @@
     (will-mount [_]
       (let [focus (om/get-state owner :focus)]
         (go-loop []
-          (let [index (<! focus)]
-            (om/transact! app :index
-              (fn [i] index))
+          (let [current (<! focus)]
+            (om/transact! app :selected
+              (fn [_] current))
             (recur)))))
     om/IRenderState
     (render-state
       [this state]
-      (dom/div #js {:className "pure-u-1-3"}
+      (apply dom/div #js {:id "main-view" :className "pure-u-1-3"}
         (om/build-all site-view (:sites app)
           {:init-state state})))))
 
-(defn contact-view [contact owner]
+(defn thumbnail-view
+  [src owner]
   (reify
     om/IRender
-    (render [this]
-      (dom/li nil (:name contact)))))
+    (render [_]
+      (dom/div #js {:className "pure-u-1-3"}
+        (dom/img #js {:src src})))))
 
+(defn thumb-urls
+  [app]
+  (map (fn [i] (-> i :thumbnail :url))
+       (:images app)))
 
-(defn contacts-view [app owner]
+(defn details-view
+  [app owner]
   (reify
     om/IRender
-    (render [this]
-      (dom/div nil
-        (dom/h2 nil "Contact list")
-        (apply dom/ul nil
-          (om/build-all contact-view (:sites app)))))))
+    (render [_]
+      (dom/div #js {:id "details" :className "pure-u-1-3"}
+        (dom/h2 #js {:id "details-title"} (:name app))
+        (apply dom/div #js {:id "photos" :className "pure-g"}
+          (om/build-all thumbnail-view (thumb-urls app)))
+        (dom/div #js {:className "description"} 
+          (dom/h2 nil "Description")
+          (dom/span nil (:description app)))))))
+
+(defn map-view
+  [app owner]
+  (reify
+    om/IDidMount
+    (did-mount [_]
+      (let [m (gm/init-map "new delhi" (om/get-node owner "map-canvas"))]
+        (om/transact! app :map-canvas 
+          (fn [_] m))))
+    om/IRender
+    (render [_]
+      (dom/div #js {:className "pure-u-1-3" :id "map-view"}
+        (dom/div #js {:id "map-canvas" :ref "map-canvas"})))))
+
+
+(defn browser-view
+  [app owner]
+  (reify 
+    om/IRender
+    (render
+      [_]
+      (dom/div #js {:className "pure-g-r" :id "container"}
+        (om/build sites-list app)
+        (om/build details-view (:selected app))
+        (om/build map-view app))))) 
 
 
 (defn attach-root
   []
-  (om/root sites-list root-state
+  (om/root browser-view root-state
     {:target (. js/document (getElementById "attach"))}))
 
