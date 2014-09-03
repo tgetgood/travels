052073112649a1e5dcdfe18bb28e577d71070827
Thomas Getgood <thomas.getgood@gmail.com>
2014-09-03 15:05:03 -0400

    Rearraging for structure.

--- src/cljs/travels/components.cljs
+++ src/cljs/travels/components.cljs
@@ -2,7 +2,7 @@
   (:require-macros [cljs.core.async.macros :refer [go go-loop]])
   (:require [travels.maps-data :as maps]
             [travels.state :refer [root-state]]
-            
+
             [cljs.core.async :refer [<! >! put! chan]]
             [om.core :as om :include-macros true]
             [om-tools.core :refer-macros [defcomponent]]
@@ -13,7 +13,7 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (defn get-std-image-src
-  [app] (-> app 
+  [app] (-> app
       :images
       first
       :standard_resolution
@@ -21,7 +21,15 @@
 
 (defn thumb-urls
   [app]
-  (map (fn [i] (-> i :thumbnail :url)) (:images app))) 
+  (map (fn [i] (-> i :thumbnail :url)) (:images app)))
+
+(defn process-map-data
+  [state]
+  (let [current-location (maps/create-marker (:user-location state) "user")
+        loc-of-interest (maps/create-marker (-> state :selected :location) "site")]
+  {:directions (-> state :directions
+                   (get (:user-location state))
+                   (get (-> state :selected :location)))}))
 
 ;;;;; Components
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -30,12 +38,12 @@
 
 (defcomponent travel-bar [props owner]
   (render [this]
-    (dom/div {:class "distance-bar"} 
-      (dom/div {:class "walk"} 
-        (str "Walk: " (-> props :walk :time) " (" 
+    (dom/div {:class "distance-bar"}
+      (dom/div {:class "walk"}
+        (str "Walk: " (-> props :walk :time) " ("
              (-> props :walk :distance) ")"))
       (dom/div {:class "drive"}
-        (str "drive " (-> props :drive :time) " (" 
+        (str "drive " (-> props :drive :time) " ("
              (-> props :drive :distance) ")")))))
 (defcomponent site-view [app owner]
   (render-state [this {:keys [focus]}]
@@ -58,9 +66,9 @@
   (render-state [this state]
     (dom/div {:id "main-view" :class "pure-u-1-3"}
       (om/build-all site-view (mapv #(val %) (:sites app))
-        {:init-state state})))) 
+        {:init-state state}))))
 
-;;;; Details Pane 
+;;;; Details Pane
 
 (defcomponent thumbnail-view [src owner]
   (render [_]
@@ -73,11 +81,11 @@
       (dom/h2 {:id "details-title"} (:name app))
       (dom/div {:id "photos" :class "pure-g"}
         (om/build-all thumbnail-view (thumb-urls app)))
-      (dom/div {:class "description"} 
+      (dom/div {:class "description"}
         (dom/h2 nil "Description")
         (dom/span nil (:description app))))))
 
-;;;; Map 
+;;;; Map
 
 (defcomponent map-view [app owner]
   (init-state [_]
@@ -102,14 +110,6 @@
       (dom/div {:id "map-canvas" :ref "map-canvas"}))))
 
 ;;;; Global App
-
-(defn process-map-data
-  [state]
-  (let [current-location (maps/create-marker (:user-location state) "user")
-        loc-of-interest (maps/create-marker (-> state :selected :location) "site")]
-  {:directions (-> state :directions 
-                   (get (:user-location state)) 
-                   (get (-> state :selected :location)))}))
 
 (defcomponent browser-view [app owner]
   (render [_]
